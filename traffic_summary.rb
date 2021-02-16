247f267b22dcd8b5c4ce441e4f619372309967bc
djixer <asv@djixer.com>
2021-02-17 02:26:04 +0300

    Add hdd_hours

--- traffic_summary.rb
+++ traffic_summary.rb
@@ -6,6 +6,7 @@
 
 time = Time.now.strftime("%Y.%m.%d\t%H:%M")
 current_bytes = File.open('/sys/class/net/enx0469f8ec943b/statistics/rx_bytes', &:readline).to_i
+hdd_hours = %x(/usr/sbin/smartctl -a /dev/sda | grep Power_On | awk '{ print $10 }')
 
 case ARGV[0]
     when "show"
@@ -17,7 +18,7 @@ case ARGV[0]
         printf("%5.1f GB", arr.sum/1000000000.0)
     when "save"
         file = File.open("./summary_traffic.txt", "a")
-        file.puts "#{time}\t#{current_bytes}"
+        file.puts "#{time}\t#{current_bytes}\t#{hdd_hours}"
         file.close
         puts "The data has been successfully saved."
     else
